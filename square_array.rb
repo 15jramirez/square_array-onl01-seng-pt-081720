5c810c61b3e7d1cad4f1af5616acd3219af4f87d
Jocelyn  Ramirez <ramirez.jocelyn73@yahoo.com>
2020-08-26 04:09:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   squred_numbers_in_array = []
   array.each do |index|
     index ** 2
-  squred_numbers_in_array.push(index**2)
+  squred_numbers_in_array.push(index*2)
 end
 squred_numbers_in_array
 end
