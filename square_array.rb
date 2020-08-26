5b72aeed74d80fa2dee98e94563c03cf9d25f869
Jocelyn  Ramirez <ramirez.jocelyn73@yahoo.com>
2020-08-26 03:42:59 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,6 @@ def square_array(array)
   squared_array =[]
   array.each do |array|
    puts array **2
-  
+      return squared_array << array **2
   end
-    return squared_array << array **2
 end
